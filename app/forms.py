660fca2a8cacc46426f4be3677618c72c3f84f59
Dee Roberts <deerob4@gmail.com>
2014-11-26 22:26:28 +0000

    Improved the layout of the form; modified typography.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField, DateField, BooleanField, SubmitField, SelectField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 
 
 class MemberForm(Form):
@@ -13,7 +13,7 @@ class MemberForm(Form):
                                                                      'Your password must be between 8-20 characters.')])
     confirm = PasswordField("Confirm your password:", validators=[DataRequired('You must confirm your password.'),
                                                                   EqualTo('password', 'Your passwords must match.')])
-    charity_event = BooleanField("Do you want to be in the team?")
+    charity_event = BooleanField("I want the chance to run in the charity event")
     distance = SelectField('What is the maximum distance you have run before?',
                            choices=[('l1', 'Less than 1 mile'), ('1-5', '1 - 5 miles'), ('6-10', '6 - 10 miles'),
                                     ('11-15', '11 - 15 miles'), ('16-20', '16 - 20 miles'),
