c41259d50382ec2b1d6d9952148f4f82e25baa85
deerob4 <deerob4@gmail.com>
2014-11-27 08:18:15 +0000

    Updated footer

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 from app.forms import MemberForm
 
 auth = Blueprint('auth', __name__)
@@ -6,4 +6,8 @@ auth = Blueprint('auth', __name__)
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     form = MemberForm()
+    if form.validate_on_submit():
+        print('woohoo')
+    for error in form.errors.items():
+        flash(error[1][0], 'warning')
     return render_template('auth/register.html', form=form)
