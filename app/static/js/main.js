024f5acb79957a5d9f35a5148d61da377aad6be5
Dee Roberts <deerob4@gmail.com>
2015-01-06 08:50:33 +0000

    Presumably done quite a lot.

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -15,8 +15,8 @@ $(document).ready(function() {
                 $('.activity-picker ul').append("<li class='activity-block'> <span>Swimming <span class='glyphicon glyphicon-ok'></span></span> <br/> <form method='POST'> <div class='form-group'>{{form.style.label}}{{form.style(class='form-control activity-input')}}</div><div class='row'> <div class='col-md-6'> <div class='form-group'>{{form.start.label}}{{form.start(class='form-control activity-input')}}</div></div><div class='col-md-6'> <div class='form-group'>{{form.finish.label}}{{form.finish(class='form-control activity-input')}}</div></div></div><div class='form-group'>{{form.rating.label}}{{form.rating(class='form-control activity-input')}}</div><div class='form-group'>{{form.thoughts.label}}{{form.thoughts(class='form-control activity-input')}}</div>{{form.submit(class='btn btn-primary')}}</form></li>")
                 console.log('swimming!');
                 break;
-                console.log('running!');
             case 'running':
+                console.log('running!');
                 break;
             case 'cycling':
                 console.log('cycling');
@@ -29,7 +29,7 @@ $(document).ready(function() {
     function expandActivity($activity) {
         //$activity.animate({ height: '520' }, { duration: 350, queue: false });
         $activity.transition({ height: '503px' });
-        $activity.find('.glyphicon').fadeIn('slow').delay(0.5)
+        $activity.find('.glyphicon').fadeIn('slow').delay(0.5);
         $activity.find('label, input, select, textarea').css('display', 'block').addClass('animated fadeIn')
     }
 
