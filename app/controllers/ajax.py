5838bc61c83e5b81ffe3cf0140f1098d8ca6aeeb
Dee Roberts <deerob4@gmail.com>
2015-01-11 18:55:03 +0000

    Added AJAX call to load in activity blocks. Modifed button CSS.

--- app/controllers/ajax.py
+++ app/controllers/ajax.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 ajax = Blueprint('ajax', __name__)
 
 
-@ajax.route('/swimming-block')
+@ajax.route('/ajax/swimming-block', methods=['POST'])
 def swimming_block():
-    return 
+    return render_template('training/swimming_block.html')
+
+
+@ajax.route('/ajax/running-block', methods=['POST'])
+def running_block():
+    return '<p>Running is coming soon...</p>'
+
+
+@ajax.route('/ajax/cycling-block', methods=['POST'])
+def cycling_block():
+    return '<p>Cycling is coming soon...</p>'
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,33 +1,28 @@
 $(document).ready(function () {
 
-    $('.activity-picker li').not('.animated').click(function () {
-        expandActivity($(this))
-    });
     $('.sport').click(function () {
-        addActivityBlock($(this).attr('id'));
+        var activity = $(this).attr('id');
+        $.ajax({
+            url: '/ajax/' + activity + '-block',
+            type: 'POST',
+            dataType: 'html',
+            success: function (data) {
+                var item = $(data);
+                $('.activity-list').append(item);
+                expandActivity(item);
+                $('.activity-block').not('.animated').click(function () {
+                    console.log('clicked');
+                    expandActivity($(this));
+                });
+            },
+            error: function (data) {
+                console.log('Something has gone wrong: ' + data);
+            }
+        });
     });
-
-    function addActivityBlock($activity) {
-        $('.activity-adder').addClass('animated fadeOutDown');
-        switch ($activity) {
-            case 'swimming':
-                console.log('swimming!');
-                break;
-            case 'running':
-                console.log('running!');
-                break;
-            case 'cycling':
-                console.log('cycling');
-                break;
-            default:
-                console.log('Activity not found - a rogue element has been introduced; the matrix has been compromised.')
-        }
-    }
 
     function expandActivity($activity) {
-        $activity.transition({
-            height: '503px'
-        });
+        $activity.transition({height: '503px'});
         $activity.find('.glyphicon').fadeIn('slow').delay(0.5);
         $activity.find('label, input, select, textarea').css('display', 'block').addClass('animated fadeIn')
     }
